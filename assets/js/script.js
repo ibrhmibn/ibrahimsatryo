18abec7bae3083b7282e5d4e9899c7618becd733
ibrhmibn <ibrahimtyo39@gmail.com>
2025-08-24 17:44:32 +0700

    mbenakne title, ganti img

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | CodeWithTanveer";
+            document.title = "Ibrahim Ibnu";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
         else {
-            document.title = "Portfolio | Tanveer Ahmad";
+            document.title = "Satryo";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
     });
